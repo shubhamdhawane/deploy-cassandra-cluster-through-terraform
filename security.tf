626d6bf5ca49d47847eeb99fa94d2faa014cba7f
shubham dhawane <121227465+shubhamdhawane@users.noreply.github.com>
2024-04-13 07:29:59 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -5,6 +5,7 @@ resource "aws_security_group" "skyage-bastion-sg" {
   name        = "bastion-sg"
   description = "Allow port 22 from anywhere"
   vpc_id      = aws_vpc.skyage.id
+
   tags = {
     "Name" = "sg-for-bastion-server"
   }
@@ -27,84 +28,73 @@ resource "aws_security_group" "skyage-bastion-sg" {
 }
 
 ###################################
-# security group for app-servers
+# security group for database-servers
 ###################################
-resource "aws_security_group" "skyage-App-SG" {
-  name        = "App-SG"
-  description = "Allow port 22 from bastion-sg and 80 from ALB-SG"
+resource "aws_security_group" "skyage-db-SG" {
+  name        = "skyage-db-SG"
+  description = "Security group for database-servers"
   vpc_id      = aws_vpc.skyage.id
+
   tags = {
-    "Name" = "sg-for-app-servers"
+    "Name" = "skyage-db-SG"
   }
+
   ingress {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
     security_groups = [aws_security_group.skyage-bastion-sg.id]
-    description     = "allow-trrafic-from-bastion-sg-only"
-  }
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.skyage-App-SG.id]
-    description     = "allow-trrafic-from-ALB-SG-only"
+    description     = "allow-traffic-from-bastion-sg-only"
   }
 
   ingress {
     from_port       = 9042
     to_port         = 9042
     protocol        = "tcp"
-    security_groups = [aws_security_group.skyage-App-SG.id]
-    description     = "allow-trrafic-from-ALB-SG-only"
+    security_groups = [aws_security_group.skyage-bastion-sg.id]
+    description     = ""
   }
 
   ingress {
     from_port       = 7000
     to_port         = 7000
     protocol        = "tcp"
-    security_groups = [aws_security_group.skyage-App-SG.id]
-    description     = "allow-trrafic-from-ALB-SG-only"
+    security_groups = [aws_security_group.skyage-bastion-sg.id]
+    description     = "allow-traffic-from-7000"
   }
 
   ingress {
     from_port       = 8000
     to_port         = 8000
     protocol        = "tcp"
-    security_groups = [aws_security_group.skyage-App-SG.id]
-    description     = "allow-trrafic-from-ALB-SG-only"
+    security_groups = [aws_security_group.skyage-bastion-sg.id]
+    description     = "allow-traffic-from-8000"
   }
+
   ingress {
     from_port       = 9142
     to_port         = 9142
     protocol        = "tcp"
-    security_groups = [aws_security_group.skyage-App-SG.id]
-    description     = "allow-trrafic-from-ALB-SG-only"
+    security_groups = [aws_security_group.skyage-bastion-sg.id]
+    description     = "allow-traffic-from-9142"
   }
+
   ingress {
     from_port       = 7199
     to_port         = 7199
     protocol        = "tcp"
-    security_groups = [aws_security_group.skyage-App-SG.id]
-    description     = "allow-trrafic-from-ALB-SG-only"
+    security_groups = [aws_security_group.skyage-bastion-sg.id]
+    description     = "allow-traffic-from-7199"
   }
 
   ingress {
     from_port       = 7001
     to_port         = 7001
     protocol        = "tcp"
-    security_groups = [aws_security_group.skyage-App-SG.id]
-    description     = "allow-trrafic-from-ALB-SG-only"
+    security_groups = [aws_security_group.skyage-bastion-sg.id]
+    description     = "allow-traffic-from-7001"
   }
-  /*
-  ingress {
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.skyage-App-SG.id]
-    description     = "allow-trrafic-from-ALB-SG-only"
-  }
-  */
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -112,7 +102,4 @@ resource "aws_security_group" "skyage-App-SG" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow All"
   }
-}
-
-
 }
