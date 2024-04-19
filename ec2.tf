0e8df429c8253246adeb66704fe337ba92b28732
shubham dhawane <121227465+shubhamdhawane@users.noreply.github.com>
2024-04-19 20:43:51 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,10 @@
-# Data source:  Get latest AMI ID for Amazon Linux2 OS
-data "aws_ami" "amazon_linux_2" {
+# Data source: Get latest AMI ID for Ubuntu 20.04 LTS
+data "aws_ami" "ubuntu_20_04" {
   most_recent = true
-  owners      = ["amazon"]
+  owners      = ["099720109477"]  # Canonical account ID for Ubuntu AMIs
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-*-gp2"]
-  }
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
   filter {
     name   = "virtualization-type"
@@ -19,82 +15,69 @@ data "aws_ami" "amazon_linux_2" {
     values = ["x86_64"]
   }
 }
-
-
-
 # Local variables
 locals {
   user_data = <<-EOT
 #!/bin/bash -xe
-
-# System Updates
-sudo yum -y update
-
-# STEP 1 - Install cassandraDB
-
-
+sudo apt-get update
+sudo apt install openjdk-11-jre-headless -y
+sudo apt-get install curl -y
+echo "deb https://downloads.apache.org/cassandra/debian/dists/41x/main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+curl https://downloads.apache.org/cassandra/KEYS | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install cassandra -y
 EOT
 }
 
-
-
-# bastion-server in public-subnet-1
+# Bastion server in public subnet 1
 resource "aws_instance" "skyage-bastion-server" {
-  ami                         = data.aws_ami.amazon_linux_2.id
+  ami                         = data.aws_ami.ubuntu_20_04.id
   instance_type               = "t2.micro"
   key_name                    = "demo"
   subnet_id                   = aws_subnet.public_subnet1.id
-  associate_public_ip_address = "true"
+  associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.skyage-bastion-sg.id]
   tags = {
     "Name" = "bastion-server"
   }
 }
 
-
-# three database-servers in private-subnets
+# Three database servers in private subnets
 resource "aws_instance" "skyage-database-server-1" {
-  ami                         = data.aws_ami.amazon_linux_2.id
+  ami                         = data.aws_ami.ubuntu_20_04.id
   instance_type               = "t2.medium"
   key_name                    = "demo"
   subnet_id                   = aws_subnet.private_subnet1.id
-  associate_public_ip_address = "false"
+  associate_public_ip_address = false
   vpc_security_group_ids      = [aws_security_group.skyage-db-SG.id]
   user_data                   = base64encode(local.user_data)
-  #user_data = file("deploy-app.sh")
   tags = {
     "Name" = "database-server-1"
   }
-  
 }
 
-
 resource "aws_instance" "skyage-database-server-2" {
-  ami                         = data.aws_ami.amazon_linux_2.id
+  ami                         = data.aws_ami.ubuntu_20_04.id
   instance_type               = "t2.medium"
   key_name                    = "demo"
   subnet_id                   = aws_subnet.private_subnet2.id
-  associate_public_ip_address = "false"
+  associate_public_ip_address = false
   vpc_security_group_ids      = [aws_security_group.skyage-db-SG.id]
   user_data                   = base64encode(local.user_data)
-  #user_data = file("deploy-app.sh")
   tags = {
     "Name" = "database-server-2"
   }
-  
 }
 
 resource "aws_instance" "skyage-database-server-3" {
-  ami                         = data.aws_ami.amazon_linux_2.id
+  ami                         = data.aws_ami.ubuntu_20_04.id
   instance_type               = "t2.medium"
   key_name                    = "demo"
   subnet_id                   = aws_subnet.private_subnet2.id
-  associate_public_ip_address = "false"
+  associate_public_ip_address = false
   vpc_security_group_ids      = [aws_security_group.skyage-db-SG.id]
   user_data                   = base64encode(local.user_data)
-  #user_data = file("deploy-app.sh")
   tags = {
     "Name" = "database-server-3"
   }
-  
 }
